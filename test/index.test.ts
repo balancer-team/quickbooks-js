7487eb368cb5dc5890e563a34fff61ebb394f618
alanhett <ahettinger@gmail.com>
2025-05-27 17:40:17 -0400

    Test for auth url

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,17 @@
-// import { QuickBooks } from '../src'
+import 'dotenv/config'
+import test from 'node:test'
+import assert from 'node:assert'
 
-// const qb = new QuickBooks({
+import { QuickBooks } from '../src'
 
-// })
+const qb = new QuickBooks({
+  clientId: process.env.INTUIT_CLIENT_ID || '',
+  clientSecret: process.env.INTUIT_SECRET || '',
+  redirectUri: process.env.INTUIT_REDIRECT_URI || '',
+  environment: 'sandbox',
+})
+
+test('Get the auth URL', async () => {
+  const authUrl = qb.getAuthUrl()
+  assert.ok(authUrl.startsWith('https://appcenter.intuit.com/connect/oauth2?client_id='))
+})
